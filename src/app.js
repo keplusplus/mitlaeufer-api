f18d830feb8b85287df19dc86e8a0fb00704b485
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 21:41:45 +0100

    Basic structure (evening commit)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Hello from inside node.')
+const express = require('express')
+const swagger = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerdoc = yaml.load('public/swagger.yaml')
+const app = express()
+const port = 8080
+
+app.use('/swagger', swagger.serve, swagger.setup(swaggerdoc))
+
+app.listen(port, () => {
+  console.log(`Listening for requests on port ${port}...`)
+})
