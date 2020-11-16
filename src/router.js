d03f3b82792cb07619fb5c5a09dc0f3de61fcafb
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 20:43:57 +0100

    Router update

--- src/router.js
+++ src/router.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const swagger = require('swagger-ui-express')
-const pJson = require('../package.json')
 const yaml = require('yamljs')
 const swaggerdoc = yaml.load('public/swagger.yaml')
-const address = `/${pJson.name}/v${pJson.version}`
 
 module.exports = () => {
     router.use('/swagger', swagger.serve, swagger.setup(swaggerdoc))
